2161f80e17df079557a1062722024f2c3d8e65d6
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-02-27 17:09:10 -0500

    Add basic contact form, normalize.css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,30 @@ import React from 'react';
 import Layout from '../components/layout';
 
 const Contact = () => {
-  return <Layout>Get @ me.</Layout>;
+  return (
+    <Layout>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Layout>
+  );
 };
 
 export default Contact;
