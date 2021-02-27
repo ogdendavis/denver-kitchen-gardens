599750fecd6e92f0c11bacea6fb6d9ff01e6bd17
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-02-27 18:27:15 -0500

    Remove MDX plugins, add markdown remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: "Denver Kitchen Gardens",
+    title: 'Denver Kitchen Gardens',
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-remark',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
+    },
+    // Make gatsby aware of CMS content
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/cms/pages`,
+      },
     },
   ],
 };
