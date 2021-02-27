2e4b9d3ba6972cd6d04e0d31a5a1e3cc072c2860
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-02-27 17:12:56 -0500

    Add dummy 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const FourOhFour = () => {
+  return (
+    <Layout>
+      Looks like you're in the wrong place wearing the wrong amount of
+      insulating layers, flatlander. No page here.
+    </Layout>
+  );
+};
+
+export default FourOhFour;
