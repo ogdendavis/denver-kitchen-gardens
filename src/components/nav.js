7f36e2ae96fc65364ff294a58abb164003cab892
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-02-28 13:01:47 -0500

    Generate links to service pages in nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 const TempNav = styled.nav`
   width: 100%;
@@ -13,12 +13,48 @@ const TempLink = styled(Link)`
 `;
 
 const Nav = () => {
+  // Get service page info for nav
+  const data = useStaticQuery(graphql`
+    query servicePageNavQuery {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "//cms/service_pages/" } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              published
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+  // Filter out unbpulished service pages
+  const servicePages = data.allMarkdownRemark.edges.filter(
+    x => x.node.frontmatter.published === true
+  );
+  // Create Links - this will change drastically once we're styling the menu
+  // Link path created in same way as slug is created in gatsby-node
+  const serviceLinks = servicePages.map(({ node }) => (
+    <TempLink
+      key={`nav-${node.frontmatter.title}`}
+      to={`/services/${node.frontmatter.title
+        .toLowerCase()
+        .split(' ')
+        .join('-')}`}
+    >
+      {node.frontmatter.title}
+    </TempLink>
+  ));
+
   return (
     <TempNav>
       <TempLink to="/">Home</TempLink>
       <TempLink to="/about">About</TempLink>
       <TempLink to="/work">Work</TempLink>
       <TempLink to="/contact">Contact</TempLink>
+      {serviceLinks}
     </TempNav>
   );
 };
