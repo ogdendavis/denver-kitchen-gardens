4362af3a256faeb7ebe3e645451e816051ed309e
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-03-20 12:53:20 -0400

    Add theme provider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import 'normalize.css';
 
@@ -6,12 +7,30 @@ import Nav from './nav';
 import Footer from './footer';
 
 const Layout = ({ children }) => {
+  // Theme for global styles
+  const theme = {
+    colors: {
+      background: '#efedeb',
+      background_dark: '#cec7b7',
+      background_light: '#fbfbfb',
+      button: '#22526c',
+      button_light: '#e3dfdc',
+      faq: '#65673b',
+      heading: '#903e23',
+      text: '#737373',
+      text_beige: '#cec7b7',
+      text_dark: '#3d3d3d',
+      text_light: '#e3dfdc',
+      text_white: '#fcfcfc',
+    },
+  };
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Nav />
       <div>{children}</div>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 
-const TempNav = styled.nav`
-  width: 100%;
-  text-align: center;
-  margin-bottom: 2rem;
+const NavContainer = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem 8vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-const TempLink = styled(Link)`
+const NavLogo = styled.img`
+  width: 150px;
+`;
+
+const NavLink = styled(Link)`
   margin: 0 0.5rem;
+  color: ${props => props.theme.colors.text};
+  text-decoration: none;
+
+  &:hover {
+    text-decoration-line: underline;
+    text-decoration-thickness: 0.1em;
+  }
 `;
 
 const Nav = () => {
+  // Grab header info from CMS
+  const data = useStaticQuery(graphql`
+    query NavQuery {
+      content: markdownRemark(
+        fileAbsolutePath: { regex: "//cms/general/site.md/" }
+      ) {
+        frontmatter {
+          header_logo
+        }
+      }
+    }
+  `);
+
+  // Pull logo path out of data
+  const logoPath = data.content.frontmatter.header_logo;
+
   return (
-    <TempNav>
-      <TempLink to="/">Home</TempLink>
-      <TempLink to="/services">Services</TempLink>
-      <TempLink to="/portfolio">Portfolio</TempLink>
-      <TempLink to="/about">About</TempLink>
-      <TempLink to="/contact">Contact</TempLink>
-    </TempNav>
+    <NavContainer>
+      <Link to="/">
+        <NavLogo src={logoPath} alt="DKG logo" />
+      </Link>
+      <div>
+        <NavLink to="/services">Services</NavLink>
+        <NavLink to="/portfolio">Portfolio</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </div>
+      <NavLink to="/contact">Contact</NavLink>
+    </NavContainer>
   );
 };
 
