3832016d0a2c5c8fec6c05a9ca656339a644e50c
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-04-22 15:13:51 -0400

    Add & style About page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,44 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
 
 import Layout from '../components/layout';
+
+const AboutContainer = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  margin: 5rem auto;
+  width: ${props => props.theme.content.width};
+  max-width: ${props => props.theme.content.maxWidth};
+`;
+
+const AboutCopy = styled.article`
+  color: ${props => props.theme.colors.text};
+  width: 47%;
+  min-width: ${props => props.theme.content.minWidth};
+
+  h1 {
+    margin-top: 0;
+  }
+`;
+
+const AboutImages = styled.div`
+  width: 48%;
+  min-width: ${props => props.theme.content.minWidth};
+`;
+
+const ProfileImage = styled.img`
+  display: block;
+  margin-bottom: 1.75rem;
+  width: 100%;
+`;
+
+const AnotherImage = styled.img`
+  display: block;
+  width: 100%;
+`;
 
 const About = () => {
   const data = useStaticQuery(graphql`
@@ -24,13 +61,30 @@ const About = () => {
     }
   `);
 
+  // Point at content object once
+  const content = data.content.frontmatter;
+
+  // Loop over more_pics to generate images
+  const moreImages = content.more_pics.map(({ image }) => (
+    <AnotherImage key={`another-${image}`} src={image} />
+  ));
+
   return (
     <Layout
-      heroImage={data.content.frontmatter.hero}
-      heroHeading={data.content.frontmatter.title}
-      heroText={data.content.frontmatter.hero_text}
+      heroImage={content.hero}
+      heroHeading={content.title}
+      heroText={content.hero_text}
     >
-      <main dangerouslySetInnerHTML={{ __html: data.content.html }} />
+      <AboutContainer>
+        <AboutCopy>
+          <h1>{content.content_heading}</h1>
+          <ReactMarkdown>{content.content_copy}</ReactMarkdown>
+        </AboutCopy>
+        <AboutImages>
+          <ProfileImage src={content.profile_pic} />
+          {moreImages}
+        </AboutImages>
+      </AboutContainer>
     </Layout>
   );
 };
