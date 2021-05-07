8e21747f4cc9dd9e123e4025dedb8d77ddfdc905
Lucas Ogden-Davis <lucas@ogdendavis.com>
2021-05-07 08:45:34 -0400

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,30 @@
-// TODO: style this page!
-
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
+const WrongPlace = styled.div`
+  margin: 3rem auto;
+  width: ${props => props.theme.content.width};
+  max-width: ${props => props.theme.content.maxWidth};
+  min-width: ${props => props.theme.content.minWidth};
+`;
+
 const FourOhFour = ({ location }) => {
   return (
-    <Layout location={location}>
-      Looks like you're in the wrong place wearing the wrong amount of
-      insulating layers, flatlander. No page here.
+    <Layout location={location} heroHeading="Oh no!">
+      <WrongPlace>
+        <p>
+          Looks like you're in the wrong place wearing the wrong amount of
+          insulating layers, flatlander. No page here.
+        </p>
+        <p>
+          Think there should be a page here?{' '}
+          <Link to="/contact">Let us know.</Link> Otherwise, use the menu above
+          to navigate to where you want to be!
+        </p>
+      </WrongPlace>
     </Layout>
   );
 };
